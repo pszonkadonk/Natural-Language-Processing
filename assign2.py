4fe9ca44d0210c3aefa6b405676336e86c25b4ea
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-02-22 18:32:01 -0500

    Initial Commit

---
+++ assign2.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+def top_words(words, scores):
+    cnt = Counter()
+    with open("sample.txt") as rd:
+        for line in rd:
+            movie_data = line.strip().split("|")
+            if(int(movie_data[3]) in scores):
+                review = movie_data[2].split(" ")
+                for word in review:
+                    if word in cnt:
+                        cnt[word] += 1
+                    else:
+                        cnt[word] = 1
+        
+    return cnt.most_common(words)
+
+
+
+foo = top_words(10, [1,2])
+
+print(foo)
