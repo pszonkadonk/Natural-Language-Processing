579ff39e3b5e9eb375c199a75cd12f3c789e9598
Michael Pszonka <pszonkadonk@Michaels-MacBook-Air.local>
2017-03-22 17:30:25 -0400

    Implement review count

--- Homework6/freelancer.py
+++ Homework6/freelancer.py
@@ -2,12 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+URL = 'https://freelancer.com'
+
+SKILLS = ['Javascript', 'PHP', 'Java', 'Python', 'MySQL', 'CSS', 'Excel', 'Accounting',
+         'Finance','Advertising', 'Marketing', 'Android', 'Swift', 'Ruby', 'HTML5', 'Perl',
+          'Photoshop', 'Illustrator', 'Photography', 'Spanish', 'German','French','Japanese', 'Hindi']
+
 
 def get_freelancer_info(freelancers):
     freelancer_information = []
     for freelancer in freelancers:
         freelancer_dict = {}
-        endpoint = url  + freelancer
+        endpoint = URL  + freelancer
         r = requests.get(endpoint)
         data = r.text
         soup = BeautifulSoup(data, 'html.parser')
@@ -15,11 +21,12 @@ def get_freelancer_info(freelancers):
         name = soup.find('h2', class_="profile-intro-username").text.strip()
         location = soup.find('span', attrs={'itemprop': 'addressLocality'}).text
         rating = soup.find('meta', attrs={'itemprop': 'ratingValue'}, content=True)['content']
-        
+        review_count = soup.find('meta', attrs={'itemprop': 'reviewCount'}, content=True)['content']
 
         freelancer_dict['location'] = location
         freelancer_dict['rating'] = rating
         freelancer_dict['name'] = name
+        freelancer_dict['review_count'] = review_count
 
 
         freelancer_information.append(freelancer_dict)
@@ -40,35 +47,30 @@ def writeToCSV(freelancer_data):
 
 
 
+def scrape_freelancer():
+    for skill in SKILLS:
+        search_freelancers = 'https://www.freelancer.com/freelancers/United_States/'+skill
+        r = requests.get(search_freelancers)
+        data = r.text
+
+        soup = BeautifulSoup(data, 'html.parser')
+
+        freelancers = []
+        for a in soup.find_all('a',attrs={'hireme-event': 'ProfileRedirect'}, href=True):
+            freelancers.append(a['href'])
+
+        freelancers = list(set(freelancers))
+
+        freelancer_info = get_freelancer_info(freelancers)
+
+        writeToCSV(freelancer_info)
 
 
-url = 'https://freelancer.com'
 
-# skills = ['Javascript', 'PHP', 'Java', 'Python', 'MySQL', 'CSS', 'Excel', 'Accounting',
-#          'Finance','Advertising', 'Marketing', 'Android', 'Swift', 'Ruby', 'HTML5', 'Perl',
-#           'Photoshop', 'Illustrator', 'Photography', 'Spanish', 'German','French','Japanese', 'Hindi']
-
-
-skills = ['Javascript', 'CSS', 'Java', 'Android', 'MySQL', 'Ruby', 'Excel', 'PHP']
-
-for skill in skills:
-    search_freelancers = 'https://www.freelancer.com/freelancers/United_States/'+skill
-    print(search_freelancers)
-    r = requests.get(search_freelancers)
-    data = r.text
-
-    soup = BeautifulSoup(data, 'html.parser')
-
-    freelancers = []
-    for a in soup.find_all('a',attrs={'hireme-event': 'ProfileRedirect'}, href=True):
-        freelancers.append(a['href'])
-
-    freelancers = list(set(freelancers))
-
-    freelancer_info = get_freelancer_info(freelancers)
-
-    writeToCSV(freelancer_info)
-
+scrape_freelancer()
     
-    
+
+
+
+
 
--- Homework6/visualize_freelancer.py
+++ Homework6/visualize_freelancer.py
@@ -1,7 +1,20 @@
 import folium
 import pandas as pd
 
-new_york_city_map = folium.Map(location=[40.7128,74.0059])
-new_york_city_map.save('osm.html')
 
+freelancer_data = pd.read_csv('freelancers.csv')
+
+
+map_1 = folium.Map(location=[29.7604,-95.3698], zoom_start=4)
+
+for index, row in freelancer_data.iterrows():
+    folium.Marker([row['latitude'], row['longitude']], popup=row['name'] + ", " +
+                     "Rating: " + str(row['rating']) + ", " +
+                     "Review Count: " + str(row['review_count']),
+                      icon = folium.Icon(icon='cloud')).add_to(map_1)
+
+
+
+map_1.save('freelance.html')
+    
 
